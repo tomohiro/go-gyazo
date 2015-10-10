1f38ad4a3a3abec9eec443900a988959821adabf
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-10-10 23:35:44 +0900

    Support list feature from Image API

--- gyazo.go
+++ gyazo.go
@@ -1,6 +1,11 @@
 package gyazo
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
 
 const (
 	version         = "0.1"
@@ -8,10 +13,13 @@ const (
 	uploadEndpoint  = "https://upload.gyazo.com"
 )
 
-// A Client manages communication with the Gyazo API
+// Client manages communication with the Gyazo API
 type Client struct {
-	// Gyazo API access token
+	// Gyazo API access token.
 	token string
+
+	// client provides request to API endpoints.
+	client *http.Client
 
 	// DefaultEndpint is Gyazo API endpoint.
 	DefaultEndpoint string
@@ -20,12 +28,52 @@ type Client struct {
 	UploadEndpoint string
 }
 
+// Image represents a uploaded image.
+//
+// Gyazo API docs: https://gyazo.com/api/docs/image
+type Image struct {
+	ID           string `json:"image_id"`
+	PermalinkURL string `json:"permalink_url"`
+	ThumbURL     string `json:"thumb_url"`
+	URL          string `json:"url"`
+	Type         string `json:"type"`
+}
+
 // NewClient returns a new Gyazo API client.
 func NewClient(token string) (*Client, error) {
 	var err error
 	if token == "" {
-		return nil, errors.New("Required access token")
+		return nil, errors.New("access token is empty")
 	}
-	c := &Client{token, defaultEndpoint, uploadEndpoint}
+
+	c := &Client{token, http.DefaultClient, defaultEndpoint, uploadEndpoint}
 	return c, err
+}
+
+// List returns user images
+func (c *Client) List() ([]Image, error) {
+	var err error
+
+	url := fmt.Sprintf("%s/api/images", c.DefaultEndpoint)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.token))
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New(res.Status)
+	}
+
+	list := new([]Image)
+	if err = json.NewDecoder(res.Body).Decode(&list); err != nil {
+		return nil, err
+	}
+	return *list, nil
 }
--- gyazo_test.go
+++ gyazo_test.go
@@ -4,6 +4,9 @@ import "testing"
 
 func TestNewClient(t *testing.T) {
 	c, err := NewClient("DUMMY_ACCESS_TOKEN")
+	if err != nil {
+		t.Error(err)
+	}
 
 	if got, want := c.DefaultEndpoint, defaultEndpoint; got != want {
 		t.Errorf("NewClient DefaultEndpoint is %v, want %v", got, want)
@@ -12,15 +15,19 @@ func TestNewClient(t *testing.T) {
 	if got, want := c.UploadEndpoint, uploadEndpoint; got != want {
 		t.Errorf("NewClient UploadEndpoint is %v, want %v", got, want)
 	}
-
-	if err != nil {
-		t.Error(err)
-	}
 }
 
 func TestNewClientWithEmptyAccessToken(t *testing.T) {
 	_, err := NewClient("")
 	if err == nil {
 		t.Error("Expected error to be returned.")
+	}
+}
+
+func TestList(t *testing.T) {
+	c, _ := NewClient("DUMMY_ACCESS_TOKEN")
+	_, err := c.List()
+	if err != nil {
+		t.Error(err)
 	}
 }
