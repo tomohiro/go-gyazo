f1ed7dee7762cae2418e0f99766138bc8de175fb
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-10-13 17:59:59 +0900

    Use stub server

--- gyazo_test.go
+++ gyazo_test.go
@@ -1,19 +1,42 @@
 package gyazo
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+var (
+	mux    *http.ServeMux
+	server *httptest.Server
+	client *Client
+)
+
+func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+	client, _ = NewClient("DUMMY_ACCESS_TOKEN")
+	client.DefaultEndpoint = server.URL
+}
+
+func teardown() {
+	server.Close()
+}
 
 func TestNewClient(t *testing.T) {
 	c, err := NewClient("DUMMY_ACCESS_TOKEN")
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("NewClient returned error: %v", err)
 	}
 
-	if got, want := c.DefaultEndpoint, defaultEndpoint; got != want {
-		t.Errorf("NewClient DefaultEndpoint is %v, want %v", got, want)
+	if actual, expected := c.DefaultEndpoint, defaultEndpoint; actual != expected {
+		t.Errorf("NewClient DefaultEndpoint is %v, want %v", actual, expected)
 	}
 
-	if got, want := c.UploadEndpoint, uploadEndpoint; got != want {
-		t.Errorf("NewClient UploadEndpoint is %v, want %v", got, want)
+	if actual, expected := c.UploadEndpoint, uploadEndpoint; actual != expected {
+		t.Errorf("NewClient UploadEndpoint is %v, want %v", actual, expected)
 	}
 }
 
@@ -25,17 +48,42 @@ func TestNewClientWithEmptyAccessToken(t *testing.T) {
 }
 
 func TestList(t *testing.T) {
-	c, _ := NewClient("DUMMY_ACCESS_TOKEN")
-	_, err := c.List(&ListOptions{Page: 2, PerPage: 50})
-	if err != nil {
-		t.Error(err)
-	}
-}
+	setup()
+	defer teardown()
 
-func TestListWithEmptyListOptions(t *testing.T) {
-	c, _ := NewClient("DUMMY_ACCESS_TOKEN")
-	_, err := c.List(nil)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/json")
+
+		// This example is response body that from https://gyazo.com/api/docs/image.
+		fmt.Fprintln(w, `[
+			{
+				"image_id": "8980c52421e452ac3355ca3e5cfe7a0c",
+				"permalink_url": "http://gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c",
+				"thumb_url": "https://i.gyazo.com/thumb/afaiefnaf.png",
+				"url": "https://i.gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c.png",
+				"type": "png",
+				"star": "true",
+				"created_at": "2014-05-21 14:23:10+0900"
+			}
+		]`)
+	})
+
+	actual, err := client.List(nil)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("List returned error: %v", err)
+	}
+
+	expected := &[]Image{{
+		ID:           "8980c52421e452ac3355ca3e5cfe7a0c",
+		PermalinkURL: "http://gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c",
+		ThumbURL:     "https://i.gyazo.com/thumb/afaiefnaf.png",
+		URL:          "https://i.gyazo.com/8980c52421e452ac3355ca3e5cfe7a0c.png",
+		Type:         "png",
+		Star:         "true",
+		CreatedAt:    "2014-05-21 14:23:10+0900",
+	}}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("List returned %+v, want %+v", actual, expected)
 	}
 }
